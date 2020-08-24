8fef84d9c12629693244782d1a7fc15a2ad1c1d0
Justin Tatman <jatatman12@gmail.com>
2020-08-24 17:53:31 -0600

    fixing helloworld package

--- HelloWorld/src/edu/nmsu/cs/helloworld/helloworld.java
+++ HelloWorld/src/edu/nmsu/cs/helloworld/helloworld.java
@@ -1,9 +1,8 @@
-package edu.nmsu.cs.helloworld;
+package edu.nmsu.cs.HelloWorld;
 
-public class helloworld{
+public class HelloWorld {
 
-   public static void main(String args[]) {
-
-      System.out.println("HelloWorld");
-   } 
+	public static void main(String args[]) {
+		System.out.println("Hello World");
+	}
 }
